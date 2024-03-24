836c9435eab49fb8ff2859ef9cd4c040ba56dcb6
HermonaDev <hermonaaddisu@gmail.com>
2024-03-24 13:48:59 +0300

    Addded essential Functionality

--- script.js
+++ script.js
@@ -1,50 +1,105 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PLANT DISEASE CLASSIFIER</title>
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--     Loads the external CSS stylesheet for the webpage -->
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <a class="github-fork-ribbon" href="https://url.to-your.repo" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
-    <h1>Plant Disease Classifier <span class="fa fa-leaf"></span></h1>
-    <hr>
+let model;
+let labels;
+let fileUpload = document.getElementById('uploadImage')
+let img = document.getElementById('image')
+let boxResult = document.querySelector('.box-result')
+let confidence = document.querySelector('.confidence')
+let pconf = document.querySelector('.box-result p')
 
-    <!-- Input element for uploading Image files (accepted extensions = .png, .jpg, .jpeg) -->
-    <input type="file" name="uploadImage" id="uploadImage" accept=".png, .jpg, .jpeg">
-    <div class="row">
-        <div class="box">
-            <!-- Image Box which displays the Uploaded image -->
-            <img src="" alt="" id="image" width="224" height="224">
-        </div>
+        
+        let progressBar = 
+            new ProgressBar.Circle('#progress', {
+            color: 'limegreen',
+            strokeWidth: 10,
+            duration: 2000, // milliseconds
+            easing: 'easeInOut'
+        });
 
-        <div class="box box-result">
-            <!--  Simple div which shows the Model Loading Status  -->
-            <div class="init_status"></div>
-            <div>
-                <h2>Your Plant is infected with :- <span class="pred_class"></span></h2>
-                
-            </div>
-            <div class="accuracy">
-                <!--  Simple progress bar which indicates the accuracy for the predicted class -->
-                <div class="progress" id="progress">
-                    <div class="inner">
-                      
-                    </div>
-                  </div>
-            </div>
-        </div>
-    </div>
+        async function fetchData(){
+            let response = await fetch('./labels.json');
+            let data = await response.json();
+            data = JSON.stringify(data);
+            data = JSON.parse(data);
+            return data;
+        }
 
+         // here the data will be return.
+        
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tensorflow/2.0.1/tf.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/progressbar.js/1.0.1/progressbar.min.js"></script>
-    <!-- Load the  External Javascript file for web app's functionality -->
-    <script src="script.js"></script>
-</body>
-</html>
+        // Initialize/Load model
+        async function initialize() {
+            let status = document.querySelector('.init_status')
+            status.innerHTML = 'Loading Model .... <span class="fa fa-spinner fa-spin"></span>'
+            model = await tf.loadLayersModel('./tensorflowjs-model/model.json');
+            status.innerHTML = 'Model Loaded Successfully  <span class="fa fa-check"></span>'
+        }
+
+        async function predict() {
+            // Function for invoking prediction
+            let img = document.getElementById('image')
+            let offset = tf.scalar(255)
+            let tensorImg =   tf.browser.fromPixels(img).resizeNearestNeighbor([224,224]).toFloat().expandDims();
+            let tensorImg_scaled = tensorImg.div(offset)
+            prediction = await model.predict(tensorImg_scaled).data();
+           
+            fetchData().then((data)=> 
+                {
+                    predicted_class = tf.argMax(prediction)
+                    
+                    class_idx = Array.from(predicted_class.dataSync())[0]
+                    document.querySelector('.pred_class').innerHTML = data[class_idx]
+                    document.querySelector('.inner').innerHTML = `${parseFloat(prediction[class_idx]*100).toFixed(2)}% SURE`
+                    console.log(data)
+                    console.log(data[class_idx])
+                    console.log(prediction)
+
+                    progressBar.animate(prediction[class_idx]-0.005); // percent
+
+                    pconf.style.display = 'block'
+
+                    confidence.innerHTML = Math.round(prediction[class_idx]*100)
+  
+                }
+            );
+            
+        }
+
+        
+
+        fileUpload.addEventListener('change', function(e){
+            
+            let uploadedImage = e.target.value
+            if (uploadedImage){
+                document.getElementById("blankFile-1").innerHTML = uploadedImage.replace("C:\\fakepath\\","")
+                document.getElementById("choose-text-1").innerText = "Change Selected Image"
+                document.querySelector(".success-1").style.display = "inline-block"
+
+                let extension = uploadedImage.split(".")[1]
+                if (!(["doc","docx","pdf"].includes(extension))){
+                    document.querySelector(".success-1 i").style.border = "1px solid limegreen"
+                    document.querySelector(".success-1 i").style.color = "limegreen"
+                }else{
+                    document.querySelector(".success-1 i").style.border = "1px solid rgb(25,110,180)"
+                    document.querySelector(".success-1 i").style.color = "rgb(25,110,180)"
+                }
+            }
+            let file = this.files[0]
+            if (file){
+                boxResult.style.display = 'block'
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.addEventListener("load", function(){
+                    
+                    img.style.display = "block"
+                    img.setAttribute('src', this.result);
+                });
+            }
+
+            else{
+            img.setAttribute("src", "");
+            }
+
+            initialize().then( () => { 
+                predict()
+            })
+        })
